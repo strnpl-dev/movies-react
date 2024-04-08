f90cd7d6c2ee54058e5fdfe311ac38075b73ddfa
Robert <strnpl.dev@gmail.com>
2024-04-08 12:20:48 +0300

    refactored code

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,90 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Search extends React.Component {
-  state = {
-    search: "",
-    type: "all",
-  };
+const Search = (props) => {
+  const { searchMovies = Function.prototype } = props;
 
-  handleKey = (event) => {
+  const [search, setSearch] = useState("");
+  const [type, setType] = useState("all");
+
+  const handleKey = (event) => {
     if (event.key === "Enter") {
-      this.props.searchMovies(this.state.search, this.state.type);
+      searchMovies(search, type);
     }
   };
 
-  handleFilter = (event) => {
-    this.setState(
-      () => ({ type: event.target.dataset.type }),
-      () => {
-        this.props.searchMovies(this.state.search, this.state.type);
-      }
-    );
+  const handleFilter = (event) => {
+    setType(event.target.dataset.type);
+    searchMovies(search, event.target.dataset.type);
   };
 
-  render() {
-    return (
-      <div className="row">
-        <div className="input-field">
-          <input
-            placeholder="Enter movie name"
-            id="email_inline"
-            type="search"
-            className="validate"
-            value={this.state.search}
-            onChange={(evt) => this.setState({ search: evt.target.value })}
-            onKeyDown={this.handleKey}
-          />
-          <button
-            className="btn search-btn"
-            onClick={() =>
-              this.props.searchMovies(this.state.search, this.state.type)
-            }
-          >
-            Search
-          </button>
-          <form className="category-form" action="#">
-            <p>
-              <label>
-                <input
-                  name="group1"
-                  type="radio"
-                  data-type="all"
-                  defaultChecked
-                  value={this.state.category}
-                  onClick={this.handleFilter}
-                />
-                <span>All</span>
-              </label>
-            </p>
-            <p>
-              <label>
-                <input
-                  name="group1"
-                  type="radio"
-                  data-type="movie"
-                  value={this.state.category}
-                  onClick={this.handleFilter}
-                />
-                <span>Movies only</span>
-              </label>
-            </p>
-            <p>
-              <label>
-                <input
-                  name="group1"
-                  type="radio"
-                  data-type="series"
-                  value={this.state.category}
-                  onClick={this.handleFilter}
-                />
-                <span>Series only</span>
-              </label>
-            </p>
-          </form>
-        </div>
+  return (
+    <div className="row">
+      <div className="input-field">
+        <input
+          placeholder="Enter movie name"
+          id="email_inline"
+          type="search"
+          className="validate"
+          value={search}
+          onChange={(evt) => setSearch(evt.target.value)}
+          onKeyDown={handleKey}
+        />
+        <button
+          className="btn search-btn"
+          onClick={() => searchMovies(search, type)}
+        >
+          Search
+        </button>
+        <form className="category-form" action="#">
+          <p>
+            <label>
+              <input
+                name="group1"
+                type="radio"
+                data-type="all"
+                defaultChecked
+                onClick={handleFilter}
+              />
+              <span>All</span>
+            </label>
+          </p>
+          <p>
+            <label>
+              <input
+                name="group1"
+                type="radio"
+                data-type="movie"
+                onClick={handleFilter}
+              />
+              <span>Movies only</span>
+            </label>
+          </p>
+          <p>
+            <label>
+              <input
+                name="group1"
+                type="radio"
+                data-type="series"
+                onClick={handleFilter}
+              />
+              <span>Series only</span>
+            </label>
+          </p>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export { Search };
